c3ecb42dc48afef3d19012948a6eff6cd45419d1
Lord <github@gor1lla.de>
2021-01-22 15:39:06 +0100

    initial commit

---
+++ Fruchthuhn_base.py
@@ -0,0 +1,24 @@
+from random import randint
+
+import pgzrun
+from pgzero.actor import Actor
+
+apple = Actor("apple")
+
+def draw():
+    screen.clear()
+    apple.draw()
+
+def place_apple():
+    apple.x = randint(10, 800)
+    apple.y = randint(10, 600)
+
+def on_mouse_down(pos):
+    if apple.collidepoint(pos):
+        print("Treffer!")
+        place_apple()
+    else:
+        print("Daneben!")
+
+place_apple()
+pgzrun.go()
---
+++ README.md
@@ -0,0 +1,6 @@
+
+
+Ideas:
+* Lebensanzeige (bei verfehlen verliert man Leben)
+* Countdown
+* Endscreen mit Score, Leben
