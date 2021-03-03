155b14d32cc4fbc652fa07821cf91255034be063
Julia Dasch <dasch@synyx.de>
2021-03-03 13:45:47 +0100

    remove a live if player do not hit the fruit

--- fruchthuhn/6_Fruchthuhn_player_lives.py
+++ fruchthuhn/6_Fruchthuhn_player_lives.py
@@ -58,14 +58,15 @@ def place_fruit():
 
 
 def on_mouse_down(pos):
-    global score, timer
-
+    global score, timer, player_lives
     if timer > 0:
         if frucht.collidepoint(pos):
             print("Treffer!")
             place_fruit()
             score += 1
         else:
+            # ziehe ein Leben ab.
+            player_lives -= 1
             print("Daneben!")
 
 
